d67da2e2d2c6086ce73c8daeeb8cc21ba3cc022a
Ian Edwards <denkyuu@gmail.com>
2016-02-09 14:30:39 -0500

    Implement first iteration of lazy list in memory.

--- client/cooks/cooks.js
+++ client/cooks/cooks.js
@@ -1,7 +1,63 @@
+//Lazy List
+
+var cooks = ['thomas', 'ian', 'jeff', 'adriana', 'dane'];
+
+var prepend = function(name) {
+  cooks.unshift(name);
+};
+
+var swap = function(i1, i2) {
+  var firstPerson = getAt(i1);
+  var secondPerson = getAt(i2);
+  setAt(i1, secondPerson);
+  setAt(i2, firstPerson);
+};
+
+var getAt = function(i) {
+  ensureIndexExists(i);
+  return cooks[i];
+};
+
+var cooked = function(name) {
+  generateNextCook();
+  cooks.shift();
+};
+
+var setAt = function(i, name) {
+  ensureIndexExists(i);
+  cooks[i] = name;
+};
+
+var ensureIndexExists = function(i) {
+  var lastIndex = array.length - 1;
+  while(i > lastIndex) {
+    generateNextCook();
+  }
+};
+
+var generateNextCook = function() {
+  var lastIndex = array.length - 1;
+  var lastCook = cooks[lastIndex];
+  return whoFollows(lastCook);
+};
+
+var whoFollows = function(name) {
+  var followers = {
+    'thomas': 'ian',
+    'ian': 'jeff',
+    'jeff': 'adriana',
+    'adriana': 'dane',
+    'dane': 'thomas'
+  };
+  return followers[name];
+};
+
+// Meteor Template
+
 Template.cook.helpers({
   name: function() {
     var index = this.index;
-    var cooks = ['thomas', 'ian', 'jeff','thomas', 'adriana', 'dane'];
+    var cooks = ['thomas', 'ian', 'jeff', 'adriana', 'dane'];
     return cooks[index];
   },
   capitalize: function(word) {
@@ -9,3 +65,8 @@ Template.cook.helpers({
   }
 });
 
+// Debug functions
+
+var debugSwapButton = function() {
+  console.log("Swap button clicked");
+};
