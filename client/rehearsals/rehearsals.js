e07df3734a130cd80a376e00f48a053172b2a4db
Ian Edwards <denkyuu@gmail.com>
2016-02-16 15:36:55 -0500

    Refactor in prep for implementing swap functionality.

--- client/rehearsals/rehearsals.js
+++ client/rehearsals/rehearsals.js
@@ -1,6 +1,68 @@
+//Lazy List
+
+var cooks = ['thomas', 'ian', 'jeff', 'adriana', 'dane'];
+
+var prepend = function(name) {
+  cooks.unshift(name);
+};
+
+var getAt = function(i) {
+  ensureIndexExists(i);
+  return cooks[i];
+};
+
+var cooked = function(name) {
+  generateNextCook();
+  cooks.shift();
+};
+
+var setAt = function(i, name) {
+  ensureIndexExists(i);
+  cooks[i] = name;
+};
+
+var ensureIndexExists = function(i) {
+  var lastIndex = array.length - 1;
+  while(i > lastIndex) {
+    generateNextCook();
+  }
+};
+
+var generateNextCook = function() {
+  var lastIndex = array.length - 1;
+  var lastCook = cooks[lastIndex];
+  return whoFollows(lastCook);
+};
+
+var whoFollows = function(name) {
+  var followers = {
+    'thomas': 'ian',
+    'ian': 'jeff',
+    'jeff': 'adriana',
+    'adriana': 'dane',
+    'dane': 'thomas'
+  };
+  return followers[name];
+};
+
 Template.body.helpers({
   rehearsals: function() {
     return Rehearsals.find({}).fetch().sort(compareStartTime).map(formatDate);
+  },
+});
+
+Template.rehearsal.helpers({
+  swap: function(i1, i2) {
+    var firstPerson = getAt(i1);
+    var secondPerson = getAt(i2);
+    setAt(i1, secondPerson);
+    setAt(i2, firstPerson);
+  },
+  cookData: function() {
+    var index = this.index;
+    var name = cooks[index];
+    var cookData = {name: name, index: this.index};
+    return cookData;
   }
 });
 
@@ -21,3 +83,9 @@ var compareStartTime = function(a, b) { //sorts calendar events by date
     return 1;
   return 0;
 };
+
+Template.rehearsal.events({
+  "click .swap-button": function(event, template) {
+    console.log("swap button clicked:" + this.index);
+  }
+});
