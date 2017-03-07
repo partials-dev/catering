db009eec1af7cb3bec5f30c6c87a46421cdfa3dd
C. Thomas Bailey <christopher.thomas.bailey@gmail.com>
2017-03-07 15:56:04 -0500

    Update to Meteor 1.4.2.7

--- server/default-user.js
+++ server/default-user.js
@@ -1,7 +1,11 @@
 import defaultUserCredentials from './gitignore/user-credentials'
 import { Accounts} from 'meteor/accounts-base'
+import stringify from 'json-stringify-safe'
 
 const existingUser = Accounts.findUserByUsername(defaultUserCredentials.username)
+
+const allAccounts = Meteor.users.find({}).fetch()
+console.log(`Existing user: ${stringify(allAccounts)}`)
 
 if (!existingUser) {
   Accounts.createUser(defaultUserCredentials)
